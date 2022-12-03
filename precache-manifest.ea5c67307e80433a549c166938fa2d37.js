c5322dedca23c05d6f7747170bd8c4452e258d88
ivansing <lancedealway@gmail.com>
2022-12-02 21:01:30 -0500

    Updates

---
+++ precache-manifest.ea5c67307e80433a549c166938fa2d37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "415c435b91cd8d7aed86",
+    "url": "/robofriends/static/css/main.3f9f7885.chunk.css"
+  },
+  {
+    "revision": "415c435b91cd8d7aed86",
+    "url": "/robofriends/static/js/main.415c435b.chunk.js"
+  },
+  {
+    "revision": "47e5e3d20373a77b7a51",
+    "url": "/robofriends/static/css/1.3bcb9031.chunk.css"
+  },
+  {
+    "revision": "47e5e3d20373a77b7a51",
+    "url": "/robofriends/static/js/1.47e5e3d2.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8432e00300e185a32d1a2b0c43030948",
+    "url": "/robofriends/index.html"
+  }
+];
